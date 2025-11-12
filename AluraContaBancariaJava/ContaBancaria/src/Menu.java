c30c542ff1785866721e74b81abfcedb592c7948
Lucas <lucass.valdez06@gmail.com>
2025-11-12 20:28:26 -0300

    refactor/otimizando-menu

--- AluraContaBancariaJava/ContaBancaria/src/Menu.java
+++ AluraContaBancariaJava/ContaBancaria/src/Menu.java
@@ -4,30 +4,25 @@ public class Menu {
     
     public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("*************************");
-        System.out.println("");
-        System.out.println("Dados iniciais do cliente: ");
-        System.out.println("");
+        System.out.println("*************************");   
+        System.out.println("Dados iniciais do cliente: \n");     
         System.out.println("Nome: Lucas Valdez");
         System.out.println("Tipo conta: Corrente");
-        System.out.println("Saldo inicial: R$ 1.500,00");
-        System.out.println("");
+        System.out.println("Saldo inicial: R$ 1.500,00\n");       
         System.out.println("*************************");
-        System.out.println("");
-
-
-            System.out.println("Operações: ");
-            System.out.println("");
-            System.out.println("1. Consultar saldos");
-            System.out.println("2. Receber valor");
-            System.out.println("3. Transferir valor");
-            System.out.println("4. Sair");
-            System.out.println("");
-            System.out.println("Escolha uma operação: ");
-
-             int opcao = scanner.nextInt();
-             int saldo = 1500; 
+           
+        int opcao =0;
+        int saldo = 1500; 
+             
              while (opcao != 4) {
+
+                System.out.println("1 - Consultar saldo");
+                System.out.println("2 - Receber transferência");
+                System.out.println("3 - Fazer transferência");
+                System.out.println("4 - Sair");
+                System.out.println("Escolha uma opção: ");
+                opcao = scanner.nextInt();
+                
                              
             switch (opcao) {
                 case 1:
@@ -44,8 +39,12 @@ public class Menu {
                       int fazTransferencia = scanner.nextInt();
                       saldo -= fazTransferencia;
                       System.out.println("Valor transferido com sucesso, seu saldo novo é: R$ " + saldo + ",00");
+                    break;
+                case 4:
+                        System.out.println("Saindo do sistema. Obrigado por usar nosso serviço!");
+                    break;
                 default: 
-                    System.out.println("Opção inválida. Tente novamente.");
+                     System.out.println("Opção inválida.");
                     break;
         }
             }   
