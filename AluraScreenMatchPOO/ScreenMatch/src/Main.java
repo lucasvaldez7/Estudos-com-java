486aa3d38252bb00cb5e71990c111f2aba357ba4
Lucas <lucass.valdez06@gmail.com>
2025-11-14 19:51:37 -0300

    test/exibindo-atributos-serie

--- AluraScreenMatchPOO/ScreenMatch/src/Main.java
+++ AluraScreenMatchPOO/ScreenMatch/src/Main.java
@@ -1,11 +1,13 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.setNome("Zootopia 2");
         meuFilme.setDuracaoEmMinutos(108);
-        meuFilme.setAnoLancamento (2025);     
+        meuFilme.setAnoLancamento (2025);
+        
 
         meuFilme.exibirFichaTecnica();
         meuFilme.avalia(8);
@@ -13,5 +15,15 @@ public class Main {
         meuFilme.avalia(10);
         
         System.out.println("A média das avaliações é: "+ meuFilme.ObterMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoLancamento(2000);
+        lost.exibirFichaTecnica();
+        lost.setTemporadas(7);
+        lost.setEpisodiosPorTemporada(10);
+        
+        
+
     }   
 }
