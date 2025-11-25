cad1b1552f42147e77f0c21d45b2926a443f549e
Lucas <lucass.valdez06@gmail.com>
2025-11-24 21:13:21 -0300

    test/testando-filtro-recomendacao-e-episodio-no-main

--- AluraScreenMatchPOO/ScreenMatch/src/Main.java
+++ AluraScreenMatchPOO/ScreenMatch/src/Main.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -28,5 +30,15 @@ public class Main {
         calculadora.inclui(lost);
         calculadora.inclui(meuFilme);
         System.out.println("Tempo total de reprodução: " + calculadora.getTempoTotal() + " minutos");
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNome("Pilot");
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(150);
+        filtro.filtra(episodio);
     }   
 }
