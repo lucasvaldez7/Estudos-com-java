af1ff043810b00257e81c59c2c8b51907d8676ae
Lucas <lucass.valdez06@gmail.com>
2025-11-24 21:11:57 -0300

    feat/adiciona-classe-filtro-recomendacao-para-sugerir-filmes

---
+++ AluraScreenMatchPOO/ScreenMatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.calculos;
+
+public class FiltroRecomendacao {
+        private String recomendacao;
+        public void filtra(Classificavel classificavel) {
+            if (classificavel.getClassificacao() >= 4) {
+                recomendacao = "Está entre os preferidos do momento!";
+            } else if (classificavel.getClassificacao() >= 2) {
+                recomendacao = "Vale a pena assistir.";
+            } else {
+                recomendacao = "Talvez você não goste muito.";
+            }
+            System.out.println(recomendacao);
+        }
+}
