39c08d224479fb1da1e2b8388db7910ae9f0f537
Lucas <lucass.valdez06@gmail.com>
2025-12-11 16:14:48 -0300

    feat/classe-exercicio-defindo-atributos-contrutores-get-set-e-toString

---
+++ MonitorDeTreino/src/br/com/lucas/academia/Exercicio.java
@@ -0,0 +1,36 @@
+package br.com.lucas.academia;
+
+public class Exercicio {
+    private String nome;
+    private double pesoMaximo;
+
+    public Exercicio(String nome, double pesoMaximo) {
+        this.nome = nome;
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPesoMaximo() {
+        return pesoMaximo;
+    }
+
+
+    public void setPesoMaximo(double pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercicio-- " +
+                "nome: " + nome + '\'' +
+                ", pesoMaximo: " + pesoMaximo +
+                '}';
+    }
+}
