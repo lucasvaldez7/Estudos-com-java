592e1dbe6142131acd4982592468fc37bb65fb48
Lucas <lucass.valdez06@gmail.com>
2025-12-11 14:08:47 -0300

    feat: adicionando-classes-CartaoDeCredito-e-Compra-para-gerenciamento-de-compras

---
+++ AluraAplicacaoDeCompras/src/br/com/alura/compras/CartaoDeCredito.java
@@ -0,0 +1,41 @@
+package br.com.alura.compras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+        public boolean lancaCompra(Compra compra) {
+
+        if (compra.getValor() <= saldo) {
+            compras.add(compra);
+            saldo -= compra.getValor();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+        
+
+}
