e3635af5d0ca0ab5c3b2679ea713fcf06db8b29f
Lucas <lucass.valdez06@gmail.com>
2025-12-12 21:45:02 -0300

    feat/adicionando-excecao-erro-de-conversao-e-modelo-titulo-omdb

--- AluraScreenMatchPOO/ScreenMatch/src/br/com/alura/screenmatch/principal/MainBusca.java
+++ AluraScreenMatchPOO/ScreenMatch/src/br/com/alura/screenmatch/principal/MainBusca.java
@@ -1,27 +1,85 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.exececao.ErroDeConversaoDeAnoException;
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class MainBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome do filme para busca: ");
-        var busca = sc.nextLine();
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=416222bf";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
+        while(!busca.equalsIgnoreCase("sair")) {
+
+            System.out.println("Digite o nome do filme para busca: ");
+            busca = sc.nextLine();
+
+            if (busca.equalsIgnoreCase("sair")){
+                break;
+            }
+
+
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=416222bf";
+            System.out.println(endereco);
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(response.body());
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
+
+
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("Titulo convertido");
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço");
+            } catch (ErroDeConversaoDeAnoException e) {
+
+                System.out.println(e.getMessage());
+            }
+            System.out.println(titulos);
+
+            FileWriter escrita =  new FileWriter("filmes.json");
+            escrita.write(gson.toJson(titulos));
+            escrita.close() ;
+
+            System.out.println("Programa finalizou corretamente! ");
+
+        }
     }
 }
